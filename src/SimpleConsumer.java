8d532eec4d361ddbed28b359eb8effd9c2c2da7a
Vinicius Pinheiro <vinic.pinheiro9@gmail.com>
2024-12-17 16:47:10 -0300

    Kafka project using java consumer and producer
    start
    zookeeper
    kafka broker
    we can also put and consume msgs using bats from kafka
    creating a topic
    .\bin\windows\kafka-topics.bat --create --topic test-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
    
    producing msgs
    .\bin\windows\kafka-console-producer.bat --topic test-topic --bootstrap-server localhost:9092
    
    consuming msgs
    .\bin\windows\kafka-console-consumer.bat --topic test-topic --bootstrap-server localhost:9092 --from-beginning

---
+++ src/SimpleConsumer.java
@@ -0,0 +1,24 @@
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class SimpleConsumer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test-group");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("test-topic"));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            records.forEach(record -> System.out.printf("Key: %s, Value: %s%n", record.key(), record.value()));
+        }
+    }
+}
---
+++ src/SimpleProducer.java
@@ -0,0 +1,21 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        for (int i = 0; i < 10; i++) {
+            producer.send(new ProducerRecord<>("test-topic", "key-" + i, "valueXXXX-" + i));
+        }
+
+        producer.close();
+    }
+}
